4f068000e45c3c3986590d4b524cc675f7e51b27
Marek Grzesiak <marekgrzesiak.22@gmail.com>
2022-06-29 14:22:45 +0000

    Remove mysql> z pliku zadanie_2.sql

--- zadanie_2.sql
+++ zadanie_2.sql
@@ -1,37 +1,37 @@
-mysql> create database zadanie_2;
+create database zadanie_2;
 
-mysql> CREATE TABLE Biblioteczka (id INT NOT NULL AUTO_INCREMENT PRIMARY
+CREATE TABLE Biblioteczka (id INT NOT NULL AUTO_INCREMENT PRIMARY
 KEY, Tytuł VARCHAR(50) NOT NULL, Data_zakupu DATE NOT NULL);
 
-mysql> INSERT INTO Biblioteczka (id, Tytuł, Data_zakupu) values (1, 'zostan olbrzymem', NOW());
+INSERT INTO Biblioteczka (id, Tytuł, Data_zakupu) values (1, 'zostan olbrzymem', NOW());
 
-mysql> SELECT * FROM Biblioteczka;
+SELECT * FROM Biblioteczka;
 +----+------------------+-------------+
 | id | Tytuł            | Data_zakupu |
 +----+------------------+-------------+
 |  1 | zostan olbrzymem | 2022-06-28  |
 +----+------------------+-------------+
 
-mysql> INSERT INTO Biblioteczka (id, Tytuł, Data_zakupu) values (2, 'how
+INSERT INTO Biblioteczka (id, Tytuł, Data_zakupu) values (2, 'how
 rich people think', NOW());
 
-mysql> INSERT INTO Biblioteczka (id, Tytuł, Data_zakupu) values (3, 'Lalka', NOW());
+INSERT INTO Biblioteczka (id, Tytuł, Data_zakupu) values (3, 'Lalka', NOW());
 
-mysql> UPDATE Biblioteczka SET Tytuł = 'Zbrodnia i kara' WHERE id=1;
+UPDATE Biblioteczka SET Tytuł = 'Zbrodnia i kara' WHERE id=1;
 
-mysql> ALTER TABLE Biblioteczka ADD Autor VARCHAR(50) AFTER Data_zakupu;
+ALTER TABLE Biblioteczka ADD Autor VARCHAR(50) AFTER Data_zakupu;
 
-mysql> UPDATE Biblioteczka SET Autor = 'Fiodor Dostojewski' WHERE id=1;
+UPDATE Biblioteczka SET Autor = 'Fiodor Dostojewski' WHERE id=1;
 
-mysql> UPDATE Biblioteczka SET Autor = 'Anthony Robbins' WHERE id=2;
+UPDATE Biblioteczka SET Autor = 'Anthony Robbins' WHERE id=2;
 
-mysql> UPDATE Biblioteczka SET Autor = 'Bolesław Prus' WHERE id=3;
+UPDATE Biblioteczka SET Autor = 'Bolesław Prus' WHERE id=3;
 
-mysql> DELETE FROM Biblioteczka WHERE id=2;
+DELETE FROM Biblioteczka WHERE id=2;
 
-mysql> DELETE FROM Biblioteczka WHERE id=3;
+DELETE FROM Biblioteczka WHERE id=3;
 
-mysql> SELECT Autor FROM Biblioteczka;
+SELECT Autor FROM Biblioteczka;
 +--------------------+
 | Autor              |
 +--------------------+
